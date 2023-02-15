7be9f615e7ce7cdfd3b0c202665b4fc5a511495c
ihsankahveci <ikahveci@uw.edu>
2023-02-15 10:26:35 -0800

    added bootstrap.R for slurm and example.R for interactive demonstration.

--- bootstrap.R
+++ bootstrap.R
@@ -1,38 +1,20 @@
-library(modeldata)
-library(tidyverse)
+library(modeldata, quietly = TRUE)
+library(tidyverse, quietly = TRUE)
 options(scipen = 99)
 set.seed(57)
 
 data(stackoverflow)
-
-## explore the data
-stackoverflow |> glimpse()
-## sanity check on the dependent variable
-hist(stackoverflow$Salary)
-
-## REGRESSION with MODEL CONFIDENCE INTERVALS
-## aka. DELTA METHOD
-fit = glm(Salary ~ -1 + Country + OpenSource + Remote + YearsCodedJob,
-          data = stackoverflow)
-
-## R has a buit-in summary function for GLM objects.
-summary(fit)
-
-## Let's create our own summary table
-## Understanding this is important for the second part.
-model_CI = confint(fit, level = 0.95) |> 
-  as_tibble(rownames = "terms") |> 
-  mutate(estimate = coef(fit), .after = "terms") 
-
 
 ## REGRESSION with BOOTSTRAP CONFIDENCE INTERVALS
 ## first define the variables
 ## then write a for loop for bootstrapping
 ## collect the results into a data.frame
 
-N = 100
-data = stackoverflow
-formula = Salary ~ -1 + Country + OpenSource + Remote + YearsCodedJob
+data = stackoverflow |> 
+  mutate(Startup = ifelse(CompanySizeNumber > 100, 0, 1))
+
+N_list = c(10, 100, 1000)
+formula = Salary ~ -1 + Country + OpenSource + Remote + +Startup + YearsCodedJob
 coef_df = list()
 
 ## from 1 to N times
@@ -40,42 +22,46 @@ coef_df = list()
 ## run the same regression model on the resampled data (boot)
 ## extract model coefficients
 ## add them to the same data.frame
-for (i in 1:N){
-  boot = slice_sample(data, prop = 1, replace = TRUE)
-  fit = glm(formula, data = boot)
-  boot_coef = as_tibble(coef(fit), rownames = "term")
-  coef_df = rbind(coef_df, boot_coef)
+for (N in N_list){
+  for (i in 1:N){
+    boot = slice_sample(data, prop = 1, replace = TRUE)
+    fit = glm(formula, data = boot)
+    coef_df = coef(fit) |> 
+      as_tibble(rownames = "term") |> 
+      mutate("N" = as.character(N))  |> 
+      bind_rows(coef_df)
+  }
+  cat(N, "bootstraps are completed.\n")
 }
 
-## check the output data.frame
-head(coef_df)
 
 ## plot the distribution of estimates
-coef_df |> 
-  ggplot(aes(x = value, y = after_stat(scaled))) + 
-  geom_density() + 
+plot = coef_df |> 
+  ggplot(aes(x = value, y = after_stat(scaled), fill = N)) + 
+  geom_density(alpha = 0.5) + 
   facet_wrap(~term, scales = "free_x") + 
-  ggtitle(paste0(N, " bootstrap samples")) +
+  ggtitle("Bootstrap samples") +
   theme_bw()
 
+ggsave(plot, filename = "bootstrap_distribution.png")
+
 ## calculate percentile CIs
-## last line orders our table alphabetically for easier comparison.
+## last line splits each N into different data.frame
 alpha = 0.05
 bootsrap_CI = coef_df |> 
-  group_by(term) |> 
+  group_by(N, term) |> 
   summarise_all(list(
     estimate = mean, 
     `2.5 %` = ~quantile(.x, probs = alpha),
     `97.5 %` = ~quantile(.x, probs = 1 - alpha))) |> 
-  arrange(term)
-  
+  group_split()
 
-## compare bootstrap CI with model CI
-model_CI
-bootsrap_CI
+## save output into RDS object
+saveRDS(bootsrap_CI, "bootstrap_output.RDS")
+cat("Bootstrap completed and saved.\n")
 
-
-
+## compare bootstrap CI with across N
+bootsrap_CI |> knitr::kable(digits = 1)
 
 
 
